8b239895da4c9d18a2506d35db0f52fce2661255
davidju4717 <jud@oregonstate.edu>
2021-11-25 00:34:34 -0800

    added comments

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template, json, redirect, jsonify, url_for, flash
+from flask import Flask, json, jsonify
 import wikipedia
 import requests
 
 app = Flask(__name__)
 
-# this is the route to the main page
+# route to the main page
 @app.route('/')
 def index():
     return "Please navigate to /[title] to use this API"
 
-# extract complete content of a page
+# route to extract complete content of a page
 @app.route('/<title>')
 def article_content(title):
-    # get the content of article. If not found, get the closest article name
+    # get the content of article. If not found, use the closest article name
     try:
         page = wikipedia.page(title, auto_suggest=False)
     except wikipedia.exceptions.DisambiguationError as e:
         title = e.options[0]
         page = wikipedia.page(title, auto_suggest=False)
+
+    # return title, url and content of the article
     result = {}
     result["title"] = page.title
     result["url"] = page.url
@@ -25,10 +27,10 @@ def article_content(title):
     return (jsonify(result), 200)
 
 
-# path that returns the summary of the article
+# route to extract summary of the article
 @app.route('/<title>/summary')
 def article_summary(title):
-    # get the summary of article. If not found, get the closest article name
+    # get the summary of article. If not found, use the closest article name
     try:
         page = wikipedia.page(title, auto_suggest=False)
         summary = wikipedia.summary(title, auto_suggest=False)
@@ -36,6 +38,8 @@ def article_summary(title):
         title = e.options[0]
         page = wikipedia.page(title, auto_suggest=False)
         summary = wikipedia.summary(title, auto_suggest=False)
+
+    # return title, url and summary of the article
     result = {}
     result["title"] = page.title
     result["url"] = page.url
@@ -43,10 +47,10 @@ def article_summary(title):
     return (jsonify(result), 200)
 
 
-# path that returns first x sentences from the summary of the article
+# route to obtain first x sentences from the summary of the article
 @app.route('/<title>/summary/<sentences>')
 def article_summary_sentences(title, sentences):
-    # get the summary of article. If not found, get the closest article name
+    # get the summary of article. If not found, use the closest article name
     try:
         page = wikipedia.page(title, auto_suggest=False)
         summary = wikipedia.summary(title, auto_suggest=False, sentences=int(sentences))
@@ -54,27 +58,32 @@ def article_summary_sentences(title, sentences):
         title = e.options[0]
         page = wikipedia.page(title, auto_suggest=False)
         summary = wikipedia.summary(title, auto_suggest=False, sentences=int(sentences))
+
+    # return title, url and summary of the article
     result = {}
     result["title"] = page.title
     result["url"] = page.url
     result["summary"] = summary
     return (jsonify(result), 200)
 
-# path that returns the url of the main image of the article
+# route to obtain url of the main image of the article
 @app.route('/<title>/image')
 def article_image(title):
-    WIKI_REQUEST = 'http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles='
 
+    # get the page of article. If not found, use the closest article name
     try:
         page = wikipedia.page(title, auto_suggest=False)
     except wikipedia.DisambiguationError as e:
         title = e.options[0]
         page = wikipedia.page(title, auto_suggest=False)
 
+    # get the url to the main image of the article
+    WIKI_REQUEST = 'http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles='
     response = requests.get(WIKI_REQUEST + title)
     json_data = json.loads(response.text)
     img_link = list(json_data['query']['pages'].values())[0]['original']['source']
 
+    # return title, article url and url of the image
     result = {}
     result["title"] = page.title
     result["url"] = page.url
