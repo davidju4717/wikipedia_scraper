621aca1a491fd83cf7fee8b8d95a8c90d5f4d874
davidju4717 <jud@oregonstate.edu>
2021-12-02 16:09:10 -0600

    elimination of duplicated code

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import requests
 
 app = Flask(__name__)
 
+# function to obtain the wiki page
+def get_wiki_page(title):
+
+    # If title not found, use the closest article name
+    try:
+        page = wikipedia.page(title, auto_suggest=False)
+    except wikipedia.exceptions.DisambiguationError as e:
+        title = e.options[0]
+        page = wikipedia.page(title, auto_suggest=False)
+
+    return page
+
 # route to the main page
 @app.route('/')
 def index():
@@ -12,82 +24,36 @@ def index():
 # route to extract complete content of a page
 @app.route('/<title>')
 def article_content(title):
-    # get the content of article. If not found, use the closest article name
-    try:
-        page = wikipedia.page(title, auto_suggest=False)
-    except wikipedia.exceptions.DisambiguationError as e:
-        title = e.options[0]
-        page = wikipedia.page(title, auto_suggest=False)
 
-    # return title, url and content of the article
-    result = {}
-    result["title"] = page.title
-    result["url"] = page.url
-    result["content"] = page.content
+    # obtain title, url and content of the article page
+    page = get_wiki_page(title)
+    result = {"title": page.title, "url": page.url, "content": page.content}
     return (jsonify(result), 200)
 
 
 # route to extract summary of the article
 @app.route('/<title>/summary')
 def article_summary(title):
-    # get the summary of article. If not found, use the closest article name
-    try:
-        page = wikipedia.page(title, auto_suggest=False)
-        summary = wikipedia.summary(title, auto_suggest=False)
-    except wikipedia.exceptions.DisambiguationError as e:
-        title = e.options[0]
-        page = wikipedia.page(title, auto_suggest=False)
-        summary = wikipedia.summary(title, auto_suggest=False)
 
-    # return title, url and summary of the article
-    result = {}
-    result["title"] = page.title
-    result["url"] = page.url
-    result["summary"] = summary
-    return (jsonify(result), 200)
-
-
-# route to obtain first x sentences from the summary of the article
-@app.route('/<title>/summary/<sentences>')
-def article_summary_sentences(title, sentences):
-    # get the summary of article. If not found, use the closest article name
-    try:
-        page = wikipedia.page(title, auto_suggest=False)
-        summary = wikipedia.summary(title, auto_suggest=False, sentences=int(sentences))
-    except wikipedia.exceptions.DisambiguationError as e:
-        title = e.options[0]
-        page = wikipedia.page(title, auto_suggest=False)
-        summary = wikipedia.summary(title, auto_suggest=False, sentences=int(sentences))
-
-    # return title, url and summary of the article
-    result = {}
-    result["title"] = page.title
-    result["url"] = page.url
-    result["summary"] = summary
+    # obtain title, url and summary of the article page
+    page = get_wiki_page(title)
+    summary = wikipedia.summary(page.title, auto_suggest=False)
+    result = {"title": page.title, "url": page.url, "summary": summary}
     return (jsonify(result), 200)
 
 # route to obtain url of the main image of the article
 @app.route('/<title>/image')
 def article_image(title):
 
-    # get the page of article. If not found, use the closest article name
-    try:
-        page = wikipedia.page(title, auto_suggest=False)
-    except wikipedia.DisambiguationError as e:
-        title = e.options[0]
-        page = wikipedia.page(title, auto_suggest=False)
-
-    # get the url to the main image of the article
+    # get the url to the main image of the article page
+    page = get_wiki_page(title)
     WIKI_REQUEST = 'http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles='
-    response = requests.get(WIKI_REQUEST + title)
+    response = requests.get(WIKI_REQUEST + page.title)
     json_data = json.loads(response.text)
     img_link = list(json_data['query']['pages'].values())[0]['original']['source']
 
     # return title, article url and url of the image
-    result = {}
-    result["title"] = page.title
-    result["url"] = page.url
-    result["image_url"] = img_link
+    result = {"title": page.title, "url": page.url, "image_url": img_link}
     return (jsonify(result), 200)
 
 if __name__ == '__main__':
